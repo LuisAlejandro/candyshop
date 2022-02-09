970a38b5ea7143edc04cafac1469585bdd8ce45d
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2022-02-09 19:36:10 -0400

    [REF] Temporarily disabling python 3.11

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        # 'Programming Language :: Python :: 3.11',
     ],
     test_suite='tests',
     tests_require=read_requirements('requirements-dev.txt')
